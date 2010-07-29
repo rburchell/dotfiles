7469fa43d2f78efe35d438edd7cfb3cc85d3bb5f
Robin Burchell <robin.burchell@collabora.co.uk>
2010-07-29 19:18:29 +0100

    Add initial versions of pastebin and screenshot.

---
+++ bin/pastebin.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/ruby
+# A lot of this taken (with gratitude) from Dan Finnie's clipboard script.
+#   See: http://ruby.pastebin.com/f5ef5f028
+
+require "gtk2"
+require "tempfile"
+
+module Clipboard
+    Gtk.init
+
+    def self.set(text)
+        text = text.to_s
+        c = Gtk::Clipboard.get(Gdk::Selection::CLIPBOARD)
+        c.set_text(text)
+        c.store
+        text
+    end
+
+    def self.get
+        c = Gtk::Clipboard.get(Gdk::Selection::CLIPBOARD)
+        c.request_text{|_, text| return text}
+        Gtk::main
+    end
+end
+
+
+def stdin_text
+    text = ""
+    loop do
+        begin
+            t = $stdin.read_nonblock(1)
+        rescue
+            break
+        else
+            text << t
+        end
+    end
+    text.chomp
+end
+
+text = stdin_text
+
+# if they don't provide anything, clipboard.
+if text.empty?
+    text = Clipboard.get
+end
+
+if text.empty?
+    puts "nothing to do"
+    Process.exit
+end
+
+Tempfile.open("pastebin") { |tf|
+    tf.write text
+    tf.close
+    fname = File.basename tf.path
+    fname += ".txt"
+    `scp #{tf.path} "w00t@dereferenced.net:/var/www/w00t.dereferenced.net/p/t/#{fname}"`
+    `ssh w00t@dereferenced.net 'chmod o+r /var/www/w00t.dereferenced.net/p/t/#{fname}'`
+    path = "http://w00t.dereferenced.net/p/t/#{fname}"
+    Clipboard::set path
+    puts "Uploaded snippet to #{path}"
+}
---
+++ bin/screenshot.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+# Some of this taken (with gratitude) from Dan Finnie's clipboard script.
+#   See: http://ruby.pastebin.com/f5ef5f028
+
+require "gtk2"
+require "tempfile"
+
+module Clipboard
+    Gtk.init
+
+    def self.set(text)
+        text = text.to_s
+        c = Gtk::Clipboard.get(Gdk::Selection::CLIPBOARD)
+        c.set_text(text)
+        c.store
+        text
+    end
+end
+
+Tempfile.open("imgbin") { |tf|
+    tf.close
+
+    # just -frame for window
+    `import -window root -frame jpg:#{tf.path}`
+    fname = File.basename tf.path
+    fname += ".jpg"
+    `scp #{tf.path} "w00t@dereferenced.net:/var/www/w00t.dereferenced.net/p/i/#{fname}"`
+    `ssh w00t@dereferenced.net 'chmod o+r /var/www/w00t.dereferenced.net/p/i/#{fname}'`
+    path = "http://w00t.dereferenced.net/p/i/#{fname}"
+    Clipboard::set path
+    puts "Uploaded screenshot to #{path}"
+}
