dfd28ff8e9b17fa4a8054be376e4cbf44cf6e9ae
Robin Burchell <robin.burchell@collabora.co.uk>
2010-09-14 16:06:25 +1000

    Simple script to copy text to clipboard.
    Useful for e.g. git diff | clipboard.rb

---
+++ bin/clipboard.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/ruby
+# A lot of this taken (with gratitude) from Dan Finnie's clipboard script.
+#   See: http://ruby.pastebin.com/f5ef5f028
+
+require "gtk2"
+require "tempfile"
+
+module Clipboard
+    Gtk.init
+
+    def self.set(text)
+        text = text.to_s
+        c = Gtk::Clipboard.get(Gdk::Selection::CLIPBOARD)
+        c.set_text(text)
+        c.store
+        text
+    end
+
+    def self.get
+        c = Gtk::Clipboard.get(Gdk::Selection::CLIPBOARD)
+        c.request_text{|_, text| return text}
+        Gtk::main
+    end
+end
+
+
+def stdin_text
+    text = ""
+    loop do
+        begin
+            t = $stdin.read_nonblock(1)
+        rescue
+            break
+        else
+            text << t
+        end
+    end
+    text.chomp
+end
+
+text = stdin_text
+
+# if they don't provide anything, clipboard.
+if !text.empty?
+    Clipboard::set text
+    puts "Copied to clipboard"
+else
+    puts "No stdin to copy"
+end
