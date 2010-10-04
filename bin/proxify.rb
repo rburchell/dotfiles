43c6cc60d096762481b7b008095972585eb165da
Robin Burchell <robin.burchell@collabora.co.uk>
2010-10-04 12:29:53 +0300

    Rewrite in ruby to gain nonblocking stdin reads easily without causing problems.

---
+++ bin/proxify.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+
+def stdin_nonblock_read
+    text = ""
+    loop do
+        begin
+            t = $stdin.read_nonblock(1)
+        rescue
+            break
+        else
+            text << t
+        end
+    end
+    text.chomp
+end
+
+def debugPrint text
+    puts "#{File.basename(__FILE__)}: #{text}"
+end
+
+# check they are actually trying to run something
+if ARGV.length == 0
+	debugPrint "you need to pass an argument"
+end
+
+stdinText = stdin_nonblock_read
+
+if `/sbin/ifconfig eth0 | /bin/grep "inet addr" | /usr/bin/awk -F: '{ print $2 }' | /usr/bin/awk '{ print $1 }' |  /bin/grep -qE '172\.21\.[0-9]{1,3}\.[0-9]{1,3}'`
+    debugPrint "tsocksifying";
+
+    # check for started ssh tunnel
+    command="ssh -fND 1234 w00t@dereferenced.net"
+    `pgrep -f -x \"#{command}\" > /dev/null 2>&1 || #{command}`
+    debugPrint "done tunnelling"
+
+    `export TSOCKS_CONF_FILE=~/.tsocks.conf`
+    puts `echo "#{stdinText}" | tsocks #{ARGV.join(" ")}`
+else
+    puts `echo "#{stdinText}" | #{ARGV.join(" ")}`
+end
