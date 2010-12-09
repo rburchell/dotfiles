324d644cf5bb5cb3f88ed914609d5b0182604b2c
Robin Burchell <robin.burchell@collabora.co.uk>
2010-12-09 00:22:55 +0000

    Add git-new-workdir

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-for i in `ls -1`; do
+for i in `ls -1 *.rb`; do
     sudo cp $i /usr/local/bin/
 done
+for i in `ls -1 *.sh`; do
+    sudo cp $i /usr/local/bin/
+done
+
+sudo cp new_workdir /usr/lib/git-core/git-new-workdir
