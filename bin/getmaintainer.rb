9742e5ed31d2b5bb110393d7f175c36d6cbd778d
Robin Burchell <viroteck@viroteck.net>
2011-01-21 13:41:04 +0000

    Ignore whitespace.

--- bin/getmaintainer.rb
+++ bin/getmaintainer.rb
@@ -4,5 +4,5 @@ require "pp"
 
 ARGV.each { |s|
     puts "Maintainers for #{s}:"
-    system("git blame #{s} | cut -d'(' -f2 | cut -d' ' -f1 | sort | uniq -c | sort -nr | head -n20")
+    system("git blame -w #{s} | cut -d'(' -f2 | cut -d' ' -f1 | sort | uniq -c | sort -nr | head -n20")
 }
