d65a6fda2aacc334686198ed9589db5088a6ea68
Robin Burchell <robin+git@viroteck.net>
2016-02-23 14:38:30 +0100

    bin: Remove a bunch of ancient/unused/duplicate scripts.

--- bin/deploy-dotfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-shopt -s dotglob
-find ~/dotfiles/.ssh -mindepth 1 -maxdepth 1 -name ".*" -exec mv {} ~/.ssh \;
-rm -r ~/dotfiles/.ssh
-find ~/dotfiles/ -mindepth 1 -maxdepth 1 -name ".*" -exec mv {} ~/.ssh \;
-mv ~/dotfiles/* ~
-rm -r ~/dotfiles
-chmod og-rwx .ssh/*
