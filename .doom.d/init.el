398fbd8f3ff0413a6eb1d7a8ad5f84d4b8ee3de6
Robin Burchell <robin.burchell@crimson.no>
2020-08-14 12:32:20 +0200

    enable qt support

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -146,7 +146,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
-       ;;qt                ; the 'cutest' gui framework ever
+       qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
