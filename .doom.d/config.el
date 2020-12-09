127d7c26032d896e4aee101de6d40ab4e97792a0
Robin Burchell <robin.burchell@crimson.no>
2020-12-09 10:03:28 +0100

    Custom hl-todo config
    Mostly because I want things like \todo \bug etc from Doxygen to be picked up.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -329,3 +329,28 @@
 ;;             tex-mode         ; latexindent is broken
 ;;             latex-mode))
 
+(after! hl-todo
+  (setq hl-todo-keyword-faces
+        `(;; For things that need to be done, just not today.
+          ("TODO" warning bold)
+          ("\\todo" warning bold)
+          ;; For problems that will become bigger problems later if not
+          ;; fixed ASAP.
+          ("FIXME" error bold)
+          ;; For tidbits that are unconventional and not intended uses of the
+          ;; constituent parts, and may break in a future update.
+          ("HACK" font-lock-constant-face bold)
+          ;; For things that were done hastily and/or hasn't been thoroughly
+          ;; tested. It may not even be necessary!
+          ("REVIEW" font-lock-keyword-face bold)
+          ;; For especially important gotchas with a given implementation,
+          ;; directed at another user other than the author.
+          ("NOTE" success bold)
+          ("\\note" success bold)
+          ;; For things that just gotta go and will soon be gone.
+          ("DEPRECATED" font-lock-doc-face bold)
+          ;; For a known bug that needs a workaround
+          ("BUG" error bold)
+          ("\\bug" error bold)
+          ;; For warning about a problematic or misguiding code
+          ("XXX" font-lock-constant-face bold))))
