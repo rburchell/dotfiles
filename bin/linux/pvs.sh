fc63e41d1e6a0954da712cfa8c97b4961f74b21d
Robin Burchell <robin.burchell@crimson.no>
2021-02-17 12:37:04 +0100

    Remove pvs, it's obsolete

--- bin/linux/pvs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pvs-studio-analyzer trace -- $@
-
-# -e /path/to/exclude-path
-pvs-studio-analyzer analyze -o $(pwd)/pvs.log -j8
-#pvs-studio-analyzer analyze --compiler=i686-pc-serenity-gcc --compiler=i686-pc-serenity-g++ -o $(pwd)/pvs.log -j8
-plog-converter -a GA:1,2 -t tasklist -o $(pwd)/pvs.tasks.log $(pwd)/pvs.log
