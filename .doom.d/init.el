cb5c47a5dea9a30d825bd37f1f246e6b68b02315
Robin Burchell <robin.burchell@crimson.no>
2025-06-21 17:07:30 +0200

    emacs: enable swift in doom

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -164,7 +164,7 @@
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       (swift +lsp)        ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
        ;;yaml              ; JSON, but readable
